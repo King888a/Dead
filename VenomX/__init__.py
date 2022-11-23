fb067ae95a59878553409e77ecc7dd72378231ed
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2022-11-23 11:41:44 +0530

    Add files via upload

---
+++ VenomX/__init__.py
@@ -0,0 +1,41 @@
+from VenomX.core.bot import VenomXBot
+from VenomX.core.dir import dirr
+from VenomX.core.git import git
+from VenomX.core.userbot import Userbot
+from VenomX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = VenomXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
