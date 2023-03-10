cfac27391ef2e65459117bce8c31ff0acee26ecb
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2023-03-10 08:29:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Itsvenomo/Venom",
+    "https://github.com/Itzvenomo/VenomXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
