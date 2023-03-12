404e38c210da85b99811eaca3d98e9b6d5727521
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2023-03-12 15:01:04 +0530

    Update userbot.py

--- VenomX/core/userbot.py
+++ VenomX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Itz_venom_family")
-                await self.one.join_chat("Itz_venom_family")
+                await self.one.join_chat("Its_Venom_family")
+                await self.one.join_chat("Its_Venom_family")
             except:
                 pass
             assistants.append(1)
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Itz_venom_family")
-                await self.two.join_chat("Itz_venom_family")
+                await self.two.join_chat("Its_Venom_family")
+                await self.two.join_chat("Its_Venom_family")
             except:
                 pass
             assistants.append(2)
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Itz_venom_family")
-                await self.three.join_chat("Itz_venom_family")
+                await self.three.join_chat("Its_Venom_family")
+                await self.three.join_chat("Its_Venom_family")
             except:
                 pass
             assistants.append(3)
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Itz_venom_family")
-                await self.four.join_chat("Itz_venom_family")
+                await self.four.join_chat("Its_Venom_family")
+                await self.four.join_chat("Its_Venom_family")
             except:
                 pass
             assistants.append(4)
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Itz_venom_family")
-                await self.five.join_chat("Itz_venom_family")
+                await self.five.join_chat("Its_Venom_family")
+                await self.five.join_chat("Its_Venom_family")
             except:
                 pass
             assistants.append(5)
